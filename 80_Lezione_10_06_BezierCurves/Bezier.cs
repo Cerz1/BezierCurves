633ed010aaef60f1a14b1c5c83ca0438e2352b6f
Cerz1 <alessiodiiorios5@gmail.com>
2024-08-12 12:21:59 +0200

    My first commit

---
+++ 80_Lezione_10_06_BezierCurves/Bezier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace _80_Lezione_10_06_BezierCurves
+{
+    static class Bezier
+    {
+        //(1 – t)P0 + tP1
+        public static Vector2 Linear(Vector2 point0, Vector2 point1, float t)
+        {
+            return (1f - t) * point0 + t * point1;
+        }
+
+        //(1 - t)^2*P0 + 2*t(1 - t)*P1 + t^2*P2
+        public static Vector2 Quadratic(Vector2 point0, Vector2 point1, Vector2 point2, float t)
+        {
+            return (float)Math.Pow(1f - t, 2) * point0 +
+                   2 * t * (1f - t) * point1 +
+                   (float)Math.Pow(t, 2) * point2;
+        }
+
+        //(1 – t)^3*P0 + 3*t*(1 - t)^2*P1 + 3*t^2*(1 - t)*P2 + t^3*P3
+        public static Vector2 Cubic(Vector2 point0, Vector2 point1, Vector2 point2, Vector2 point3, float t)
+        {
+            return (float)Math.Pow(1f - t, 3) * point0 +
+                   3 * t * (float)Math.Pow(1f - t, 2) * point1 +
+                   3 * (float)Math.Pow(t, 2) * (1 - t) * point2 +
+                   (float)Math.Pow(t, 3) * point3;
+        }
+    }
+}
---
+++ 80_Lezione_10_06_BezierCurves/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Aiv.Fast2D;
+using OpenTK;
+
+namespace _80_Lezione_10_06_BezierCurves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window window = new Window(800, 600, "Bezier");
+
+            // Movement Speed for each Control Point
+            float speed = 100;
+
+            // Control Points
+            Vector2 point0 = new Vector2(10, 250);
+            Vector2 point1 = new Vector2(200, 250);
+            Vector2 point2 = new Vector2(300, 250);
+            Vector2 point3 = new Vector2(400, 250);
+
+            // Sprites used for Rendering Points and Curve (made of lots of tiny points)
+            Sprite ctrlPoint = new Sprite(10, 10);
+            Sprite curvePoint = new Sprite(2, 2);
+
+            // Main Loop
+            while (window.IsOpened)
+            {
+                if (window.GetKey(KeyCode.Left)) point1 += new Vector2(-1f, 0) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.Right)) point1 += new Vector2(1f, 0) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.Up)) point1 += new Vector2(0f, -1) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.Down)) point1 += new Vector2(0f, 1) * speed * window.DeltaTime;
+
+                if (window.GetKey(KeyCode.A)) point2 += new Vector2(-1f, 0) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.D)) point2 += new Vector2(1f, 0) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.W)) point2 += new Vector2(0f, -1) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.S)) point2 += new Vector2(0f, 1) * speed * window.DeltaTime;
+
+                if (window.GetKey(KeyCode.F)) point3 += new Vector2(-1f, 0) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.H)) point3 += new Vector2(1f, 0) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.T)) point3 += new Vector2(0f, -1) * speed * window.DeltaTime;
+                if (window.GetKey(KeyCode.G)) point3 += new Vector2(0f, 1) * speed * window.DeltaTime;
+
+                for (float t = 0; t <= 1f; t += 0.001f)
+                {
+                    // DRAW BEZIER CURVES
+
+                    // Get each point interpolated position from Bezier formulas
+                    //Vector2 pointI = Bezier.Linear(point0, point1, t);
+                    //Vector2 pointI = Bezier.Quadratic(point0, point1, point2, t);
+                    Vector2 pointI = Bezier.Cubic(point0, point1, point2, point3, t);
+                    
+                    // Set and Draw each Curve Point
+                    curvePoint.position = pointI;
+                    curvePoint.DrawColor(0, 1f, 0);
+                }
+
+                // Set the Sprite for each Control Point and Draw it (same sprite used 4 times)
+                ctrlPoint.position = point0;
+                ctrlPoint.DrawColor(1f, 0, 0);
+
+                ctrlPoint.position = point1;
+                ctrlPoint.DrawColor(0, 0, 1f);
+
+                ctrlPoint.position = point2;
+                ctrlPoint.DrawColor(0, 1f, 1f);
+
+                ctrlPoint.position = point3;
+                ctrlPoint.DrawColor(1f, 1f, 0f);
+
+
+                window.Update();
+            }
+        }
+    }
+}
